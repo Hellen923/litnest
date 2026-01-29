22c2a2808c2a33352bde3806b825ff6aa001fe75
Hellen923 <hellenkiwagama@gmail.com>
2026-01-29 20:44:00 +0300

    Update novels URLs to use novels_home

--- novels/urls.py
+++ novels/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.novels_home, name='novels_home'),
     path('novel/<int:novel_id>/', views.novel_detail, name='novel_detail'),
     path('novel/<int:novel_id>/bookmark/', views.toggle_bookmark, name='toggle_bookmark'),
 ]
