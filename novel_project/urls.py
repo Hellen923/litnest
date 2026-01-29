8cf09f07d2acf694e5e1f723f87042f7d3bd7938
Hellen923 <hellenkiwagama@gmail.com>
2026-01-29 20:44:49 +0300

    Update main URLs to include landing at root and novels at /novels/

--- novel_project/urls.py
+++ novel_project/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin 
+from django.contrib import admin
 from django.urls import path, include
 from novels import views
 from django.conf import settings
@@ -22,10 +22,12 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('novels.urls')),  # Include URLs from the novels app
+    path('', views.landing, name='landing'),
+    path('novels/', include('novels.urls')),  # Include URLs from the novels app
+    path('accounts/', include(('django.contrib.auth.urls', 'auth'), namespace='accounts')),
     #path('', views.home, name='home'),
     #path('novel/<int:novel_id>/', views.novel_detail, name='novel_detail'),
-    
+
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
