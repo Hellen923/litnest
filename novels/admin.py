547c18bfd1a6eb2b368f743afe6ec5db66b2fc94
Hellen923 <hellenkiwagama@gmail.com>
2026-01-29 10:17:00 +0300

    Add ReadingProgress admin

--- novels/admin.py
+++ novels/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Novel, Bookmark
+from .models import Novel, Bookmark, ReadingProgress
 
 class NovelAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'genre')
@@ -10,5 +10,10 @@ class BookmarkAdmin(admin.ModelAdmin):
     list_display = ('user', 'novel', 'created_at')
     search_fields = ('user__username', 'novel__title')
 
+class ReadingProgressAdmin(admin.ModelAdmin):
+    list_display = ('user', 'novel', 'progress', 'last_read')
+    search_fields = ('user__username', 'novel__title')
+
 admin.site.register(Novel, NovelAdmin)
 admin.site.register(Bookmark, BookmarkAdmin)
+admin.site.register(ReadingProgress, ReadingProgressAdmin)
