7b3e6d1b2a5fe3dc913140384d8a30360f4247b6
Hellen923 <hellenkiwagama@gmail.com>
2026-01-29 10:04:42 +0300

    Add bookmark URL

--- novels/urls.py
+++ novels/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('novel/<int:novel_id>/', views.novel_detail, name='novel_detail'),
+    path('novel/<int:novel_id>/bookmark/', views.toggle_bookmark, name='toggle_bookmark'),
 ]
