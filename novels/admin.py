f7df241c4075b8bb542b04a0fe34f9045cf6aacb
Hellen923 <hellenkiwagama@gmail.com>
2026-01-29 10:09:32 +0300

    Add Bookmark admin

--- novels/admin.py
+++ novels/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from .models import Novel
+from .models import Novel, Bookmark
 
 class NovelAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'genre')
     search_fields = ('title', 'author')
     list_filter = ('genre',)
 
+class BookmarkAdmin(admin.ModelAdmin):
+    list_display = ('user', 'novel', 'created_at')
+    search_fields = ('user__username', 'novel__title')
+
 admin.site.register(Novel, NovelAdmin)
+admin.site.register(Bookmark, BookmarkAdmin)
